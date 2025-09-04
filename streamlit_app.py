6e594bec4fb3a2b6a02fd55efdcaa349897a8a4e
shindevijayh-ops <shindevijayh@gmail.com>
2025-09-04 11:05:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -36,5 +36,7 @@ if ingredients_list:
     if time_to_insert: 
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-    
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
